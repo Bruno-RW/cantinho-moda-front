ac7c23ef4576862087bc9c56fa7b4a9089f89c2b
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-08-27 21:16:35 -0300

    Navbar content changes

---
+++ components/header/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import NavContent from "./NavContent";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul className="flex items-center justify-center m-0">
+                <NavContent />
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
