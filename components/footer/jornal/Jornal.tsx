c96feb7b20e0dc03cc0f174bddc04ba61a86f1bd
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-08-27 21:14:42 -0300

    Add Jornal styling responsivity

--- components/footer/jornal/Jornal.tsx
+++ components/footer/jornal/Jornal.tsx
@@ -2,10 +2,10 @@ import JornalForm from "./JornalForm";
 
 const Jornal = () => {
   return (
-    <section id="jornal" className="flex flex-wrap items-center justify-between py-10 px-20 my-10 bg-[#041E42] bg-no-repeat bg-[url('/images/banners/b7.png')]">
+    <section id="jornal" className="flex flex-col lg:flex-row flex-wrap items-center justify-between text-center lg:text-start py-10 px-20 my-10 bg-[#041E42] bg-no-repeat bg-[url('/images/banners/b7.png')]">
       <div>
-          <h4 className="text-white text-[1.375rem] font-bold">Inscreva-se para receber notícias</h4>
-          <p className="text-[#818EA0] text-sm font-semibold">Receba atualizações no e-mail sobre nossa loja e <span className="text-[#FFBD27]">ofertas especiais</span></p>
+        <h4 className="text-white text-[1.375rem] font-bold">Inscreva-se para receber notícias</h4>
+        <p className="text-[#818EA0] text-sm font-semibold mb-5 lg:mb-0">Receba atualizações no e-mail sobre nossa loja e <span className="text-[#FFBD27]">ofertas especiais</span></p>
       </div>
 
       <JornalForm />
