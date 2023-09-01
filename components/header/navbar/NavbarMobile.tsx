bf365745a749763f3ff224a6dd135b8ec21febde
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-01 13:39:04 -0300

    Fixed NavbarMobile content

--- components/header/navbar/NavbarMobile.tsx
+++ components/header/navbar/NavbarMobile.tsx
@@ -1,12 +1,18 @@
-import Link from "next/link";
-import { GrClose } from "react-icons/gr";
+"use client"
+
+import { TbArrowBarRight } from "react-icons/tb";
+
+import NavContent from "./NavContent";
 
 const NavbarMobile = () => {
     return (
-        <nav>
-            <Link href="#" id="fechar" className="sm:hidden absolute top-[30px] left-[30px] text-2xl text-[#222]">
-                <GrClose size={22}/>
-            </Link>
+        <nav id="nav-mobile" className="flex fixed top-0 right-0 items-start justify-start w-[55vw] h-screen pt-20 pl-[10px] z-50 bg-[#E3E6F3] transition duration-300">
+            <button type="button" id="fechar" className="md:hidden absolute top-7 left-7 text-[#222] mb-6" onClick={() => {}}>
+                <TbArrowBarRight size={32}/>
+            </button>
+            <ul className="flex flex-col items-start justify-center">
+                <NavContent navUser={false} listClass="text-2xl mb-6" linkClass="" />
+            </ul>
         </nav>
     );
 }
