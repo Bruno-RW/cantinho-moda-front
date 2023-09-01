15ecfc66e35fbce382f9a20d6d4b60baaf53922e
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-01 13:39:43 -0300

    Navbar content to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,33 @@
 "use client";
+import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
-import Link from "next/link";
-import Image from "next/image";
-
-import useMediaQuery from "@/hooks/useMediaQuery";
-
-import Navbar from "./header/navbar/Navbar";
+import Logo from "./Logo";
+import NavUser from "./header/navbar/NavUser";
+import NavContent from "./header/navbar/NavContent";
 import NavbarMobile from "./header/navbar/NavbarMobile";
 
 const Header = () => {    
-    const isMobile = useMediaQuery("(max-width: 768px)");
+    const isMobile = useMediaQuery({query: "(max-width: 768px)"});
+    const [sideNav, setSideNav] = useState(false);
 
     return (
         <>
             <header className="sticky flex top-0 items-center justify-between w-screen z-50 py-[10px] sm:py-5 px-[30px] sm:px-20 bg-[#E3E6F3]">
-                <Link href="/" aria-label="Logo">
-                    <Image src="/images/icons/logos/logo-black.png" className="logo min-w-[120px] sm:min-w-[160px]" width={160} height={160} alt="Logo" />
-                </Link>
-
-                <Navbar />
+                <Logo src="/images/icons/logos/logo-black-lg.png" className="min-w-[120px] sm:min-w-[120px]" size={120} />
+                <nav>
+                    <ul className="flex items-center justify-center m-0">
+                        {isMobile ? (
+                            <>
+                                <NavUser href="/login" />
+                                <li className="md:hidden cursor-pointer" onClick={() => setSideNav(!sideNav)}>
+                                    <FaBars className="absolute top-[35.5px] right-[80px]" size={28} />
+                                </li>
+                            </>
+                        ) : (<NavContent navUser={true} />)}
+                    </ul>
+                </nav>
             </header>
             {isMobile && <NavbarMobile />} 
         </>
