66f7f7de851998f70ca6f4b35778a00d7a5a8aef
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-20 15:26:03 -0300

    Fixed jornal autocomplete

--- components/footer/jornal/JornalForm.tsx
+++ components/footer/jornal/JornalForm.tsx
@@ -2,7 +2,8 @@ const JornalForm = () => {
   return (
     <div className="flex w-[80%] lg:w-[40%]">
       <input type="email" id="emailNews" name="email" className="text-sm w-full h-[3.125rem] px-2 outline-none rounded rounded-tr-none
-      rounded-br-none" placeholder="Seu endereço de e-mail" required />
+      rounded-br-none" placeholder="Seu endereço de e-mail" autoComplete="email" required />
+
       <button type="button" className="text-white text-sm font-semibold py-[15px] px-[30px] max-h-[3.125rem] whitespace-nowrap bg-[#088178] rounded rounded-tl-none rounded-bl-none normal">Inscreva-se</button>
     </div>
   );
