35d8dc008a322f7249418490800e363924f7fb91
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-20 15:25:45 -0300

    Changed variable name

--- components/header/navbar/NavUser.tsx
+++ components/header/navbar/NavUser.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { usePathname } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import Link from "next/link";
@@ -7,7 +9,7 @@ import { AiOutlineUser } from "react-icons/ai";
 interface NavUserProps { className?: string }
 
 const NavUser: React.FC<NavUserProps> = ({ className }) => {
-    const pathname = (usePathname() === "/login");
+    const pathName = (usePathname() === "/login");
     const user = false;
 
     //TODO: handleOnClick();
@@ -16,13 +18,13 @@ const NavUser: React.FC<NavUserProps> = ({ className }) => {
         <>
             {user ? (
                 <li className={twMerge("relative list-none px-5 text-[#1A1A1A] text-xl lg:text-base font-semibold no-underline", className)}>
-                    <button className={twMerge(pathname && "text-[#B79130] after:absolute after:content-[''] after:w-[30%] after:h-[2px] after:left-5 after:bottom-[-4px] after:bg-[#B79130]")} onClick={() => {}}>
+                    <button className={twMerge(pathName && "text-[#B79130] after:absolute after:content-[''] after:w-[30%] after:h-[2px] after:left-5 after:bottom-[-4px] after:bg-[#B79130]")} onClick={() => {}}>
                         <AiOutlineUser size={28} />
                     </button>
                 </li>
             ) : (
                 <li className={twMerge("relative list-none px-5 text-[#1A1A1A] text-xl lg:text-base font-semibold no-underline", className)}>
-                    <Link href="/login" className={twMerge(pathname && "text-[#B79130] after:absolute after:content-[''] after:w-[30%] after:h-[2px] after:left-5 after:bottom-[-4px] after:bg-[#B79130]")}>
+                    <Link href="/login" className={twMerge(pathName && "text-[#B79130] after:absolute after:content-[''] after:w-[30%] after:h-[2px] after:left-5 after:bottom-[-4px] after:bg-[#B79130]")}>
                         <AiOutlineUser size={28} />
                     </Link>
                 </li>
