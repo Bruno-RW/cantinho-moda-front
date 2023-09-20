f05de5063d7c05af6bb96997e64ab829d8853383
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-20 16:25:01 -0300

    Fixed jornal responsivity

--- components/footer/jornal/Jornal.tsx
+++ components/footer/jornal/Jornal.tsx
@@ -2,10 +2,10 @@ import JornalForm from "./JornalForm";
 
 const Jornal = () => {
   return (
-    <section id="jornal" className="flex flex-col lg:flex-row flex-wrap items-center justify-between text-center lg:text-start py-10 px-20 my-10 bg-[#041E42] bg-no-repeat bg-[url('/images/banners/b7.png')]">
+    <section id="jornal" className="flex flex-col xl:flex-row flex-wrap items-center justify-between text-center xl:text-start py-10 px-12 sm:px-16 md:px-20 my-10 bg-[#041E42] bg-no-repeat bg-[url('/images/banners/b7.png')]">
       <div>
         <h4 className="text-white text-[1.375rem] font-bold">Inscreva-se para receber notícias</h4>
-        <p className="text-[#818EA0] text-sm font-semibold mb-5 lg:mb-0">Receba atualizações no e-mail sobre nossa loja e <span className="text-[#FFBD27]">ofertas especiais</span></p>
+        <p className="text-[#818EA0] text-sm font-semibold mb-5 xl:mb-0">Receba atualizações no e-mail sobre nossa loja e <span className="text-[#FFBD27]">ofertas especiais</span></p>
       </div>
 
       <JornalForm />
--- components/footer/jornal/JornalForm.tsx
+++ components/footer/jornal/JornalForm.tsx
@@ -1,10 +1,10 @@
 const JornalForm = () => {
   return (
-    <div className="flex w-[80%] lg:w-[40%]">
-      <input type="email" id="emailNews" name="email" className="text-sm w-full h-[3.125rem] px-2 outline-none rounded rounded-tr-none
+    <div className="flex w-full sm:w-[90%] md:w-[70%] xl:w-[40%]">
+      <input type="email" id="emailNews" name="email" className="text-sm w-full h-12 px-2 outline-none rounded rounded-tr-none
       rounded-br-none" placeholder="Seu endereço de e-mail" autoComplete="email" required />
 
-      <button type="button" className="text-white text-sm font-semibold py-[15px] px-[30px] max-h-[3.125rem] whitespace-nowrap bg-[#088178] rounded rounded-tl-none rounded-bl-none normal">Inscreva-se</button>
+      <button type="button" className="align-middle text-white text-sm font-semibold leading-3 py-2 px-4 sm:px-5 whitespace-nowrap bg-[#088178] rounded rounded-tl-none rounded-bl-none normal">Inscreva-se</button>
     </div>
   );
 }
