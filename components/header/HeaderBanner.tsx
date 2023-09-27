390a8221abdf56e7823cb85aee40b389acff2a5f
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-27 15:04:32 -0300

    Header styling improvement

--- components/header/HeaderBanner.tsx
+++ components/header/HeaderBanner.tsx
@@ -3,8 +3,8 @@ interface HeaderBannerProps {title: string; text: string}
 const HeaderBanner: React.FC<HeaderBannerProps> = ({ title, text }) => {
   return (
     <section className="flex flex-col text-center justify-center p-[14px] h-[40vh] w-full bg-cover bg-[url('/images/banners/b8.png')]">
-      <h2 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold">{title}</h2>
-      <p className="text-white text-sm md:text-base lg:text-lg mt-8">{text}</p>
+      <h2 className="text-white text-5xl lg:text-6xl font-bold">{title}</h2>
+      <p className="text-white text-base lg:text-lg mt-8">{text}</p>
     </section>
   );
 }
