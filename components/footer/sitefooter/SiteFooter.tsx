afc1549b1052b0debc84c28d1ce86ce7d0df8d9c
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-29 14:21:51 -0300

    Payment methods styling fix

--- components/footer/sitefooter/SiteFooter.tsx
+++ components/footer/sitefooter/SiteFooter.tsx
@@ -1,8 +1,9 @@
 import { storeAccount, storeContact, storeInfo, storePaymentMethods, storeSocials } from "@/lib/data";
 
 import Logo from "@/components/Logo";
-import ColumnTitle from "./ColumnTitle";
 import MapContent from "@/components/MapContent";
+
+import ColumnTitle from "./ColumnTitle";
 import ContentColumn from "./ContentColumn";
 import Copyright from "./Copyright";
 
@@ -11,6 +12,7 @@ const SiteFooter = () => {
         <section id="footer" className="section-p1 flex flex-wrap justify-between columns-4 pt-5 lg:pt-10 pb-3 lg:pb-5">
             <div className="flex flex-col items-start max-w-[200px] lg:max-w-sm">
                 <Logo className="mb-[30px] max-w-[150px] lg:max-w-xs" height={120} />
+
                 <ColumnTitle title="Contact" />
                 <MapContent data={storeContact} type="contact" />
             </div>
@@ -24,7 +26,7 @@ const SiteFooter = () => {
             </ContentColumn>
 
             <ContentColumn title="Payment Methods">
-                <div className="flex">
+                <div className="flex flex-wrap">
                     <MapContent data={storePaymentMethods} type="payment" />
                 </div>
 
